21dd2ee8638ba18dfaa0b2f0b7ebda365b443ecf
vanpipy <vanpipy@gmail.com>
2025-04-23 17:51:23 +0800

    refactor(fibonacci): optimize fibonacci calculation using memoization

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,28 +1,25 @@
 def fibonacci_number(n):
-    if n <= 1:
-        return n
+    a = { 0: 0, 1: 1, 2: 1, 3: 2, 4: 3, 5: 5, 6: 8 }
+    index = n
 
-    if n == 2:
-        return 1
+    if n <= 6:
+        return a[n]
 
-    if n == 3:
-        return 2
+    c = [n]
 
-    i = 2
-    a = [0, 1, 1, 2]
+    while n // 2 >= 2:
+        c.append(n // 2)
+        n = n // 2
 
-    while True:
-        next_a_even = a[i] * (2 * a[i + 1] - a[i])
-        next_a_odd = a[i] ** 2 + a[i + 1] ** 2
-        a.append(next_a_even)
-        a.append(next_a_odd)
+    while len(c) > 1:
+        x0 = c.pop()
+        x1 = c[len(c) - 1]
+        if x1 % 2 == 0:
+            a[x1] = a[x0] * (2 * a[x0 + 1] - a[x0])
+        else:
+            a[x1] = a[x0] ** 2 + a[x0 + 1] ** 2
 
-        if i * 2 == n:
-            return next_a_even
-        elif i * 2 + 1 == n:
-            return next_a_odd
-
-        i += 1
+    return a[index]
 
 def fibonacci_sum(n):
     _sum = fibonacci_number(n + 2) - 1
--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -2,30 +2,27 @@
 import sys
 
 def fibonacci_number(n):
-    if n <= 1:
-        return n
+    a = { 0: 0, 1: 1, 2: 1, 3: 2, 4: 3, 5: 5, 6: 8 }
+    index = n
 
-    if n == 2:
-        return 1
+    if n <= 6:
+        return a[n]
 
-    if n == 3:
-        return 2
+    c = [n]
 
-    i = 2
-    a = [0, 1, 1, 2]
+    while n // 2 >= 2:
+        c.append(n // 2)
+        n = n // 2
 
-    while True:
-        next_a_even = a[i] * (2 * a[i + 1] - a[i])
-        next_a_odd = a[i] ** 2 + a[i + 1] ** 2
-        a.append(next_a_even)
-        a.append(next_a_odd)
+    while len(c) > 1:
+        x0 = c.pop()
+        x1 = c[len(c) - 1]
+        if x1 % 2 == 0:
+            a[x1] = a[x0] * (2 * a[x0 + 1] - a[x0])
+        else:
+            a[x1] = a[x0] ** 2 + a[x0 + 1] ** 2
 
-        if i * 2 == n:
-            return next_a_even
-        elif i * 2 + 1 == n:
-            return next_a_odd
-
-        i += 1
+    return a[index]
 
 def fibonacci_partial_sum_naive(from_, to):
     from_sum = fibonacci_number(from_ + 2) - 1
