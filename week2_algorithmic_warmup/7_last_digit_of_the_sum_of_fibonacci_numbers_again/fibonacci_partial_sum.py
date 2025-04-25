f9d0e0b041dacbace4e4a9c6b0391169e933f26f
vanpipy <vanpipy@gmail.com>
2025-04-25 22:54:08 +0800

    refactor(fibonacci): optimize and simplify Fibonacci calculation logic

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -2,25 +2,39 @@
 import sys
 
 def fibonacci_number(n):
-    a = { 0: 0, 1: 1, 2: 1, 3: 2, 4: 3, 5: 5, 6: 8 }
+    a = { 0: 0, 1: 1, 2: 1 }
     index = n
 
-    if n <= 6:
+    if n <= 2:
         return a[n]
 
     c = [n]
 
-    while n // 2 >= 2:
+    while n >= 6:
         c.append(n // 2)
         n = n // 2
 
-    while len(c) > 1:
-        x0 = c.pop()
-        x1 = c[len(c) - 1]
-        if x1 % 2 == 0:
-            a[x1] = a[x0] ** 2 + 2 * a[x0] * a[x0 - 1]
-        else:
-            a[x1] = 2 * a[x0] ** 2 + a[x0 - 1] ** 2 + 2 * a[x0] * a[x0 - 1]
+    # a[2 * k] = a[k] * (2 * a[k + 1] - a[k])
+    # a[2 * k + 1] = a[k + 1] ** 2 + a[k] ** 2
+    while len(c) > 0:
+        x = c.pop()
+        k = x // 2
+
+        if a.get(k) is None:
+            c.append(x)
+            c.append(k)
+            continue
+
+        if a.get(k + 1) is None:
+            c.append(x)
+            c.append(k + 1)
+            continue
+
+        if a.get(x) is None:
+            if x % 2 == 0:
+                a[x] = a[k] * (2 * a[k + 1] - a[k])
+            else:
+                a[x] = a[k + 1] ** 2 + a[k] ** 2
 
     return a[index]
 
